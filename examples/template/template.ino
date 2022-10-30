cabc1ae807939e385df66a31ee84b2d5f3e87776
HayatoTajiri <tajiri1999@gmail.com>
2022-10-30 12:16:24 +0900

    modify template

--- examples/template/template.ino
+++ examples/template/template.ino
@@ -1,12 +1,10 @@
 #include <takiroboF1.h>
 
 //HMC5883L(地磁気), MPU(ジャイロ), NONE(無し) から選択する
-//何も指定しない場合, コンパスは使用されない
-takiroboF1 robot(MPU);
+takiroboF1 robot;
 
 void setup()
 {
-  //ここは電源を入れてから一度しか実行されません。
   Serial.begin(9600);
   robot.init();
   
@@ -14,9 +12,8 @@ void setup()
 
 void loop()
 {
-  //プログラムはここをループし続けます。
   if(robot.getBtn() == HIGH)
   {
-    //ここにプログラムを書けば、スタートスイッチを押した時だけロボットが動作する
+    
   }
 }
